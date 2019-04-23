a24d47bf9f10859fd7cf364db918adcfd4654b74
Neil LaHaise <nlahaise@telus.net>
2019-04-23 09:58:20 -0700

    added authorization

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import { PasswordForgetForm} from '../PasswordForget';
+import { AuthUserContext, withAuthorization } from '../Session';
+import { PasswordForgetForm } from '../PasswordForget';
 import PasswordChangeForm from '../PasswordChange';
+//import { fromEventPattern } from 'rxjs';
 
 const AccountPage = () => (
-  <div>
-    <h1>Account Page</h1>
-    <PasswordForgetForm />
-    <PasswordChangeForm />
-  </div>
+  <AuthUserContext.Consumer>
+    {authUser => (
+      <div>
+        <h1>Account: {authUser.email}</h1>
+        <PasswordForgetForm />
+        <PasswordChangeForm />
+      </div>
+    )}
+  </AuthUserContext.Consumer>
 );
 
-export default AccountPage;
+const condition = authUser => !!authUser;
+
+export default withAuthorization(condition)(AccountPage);

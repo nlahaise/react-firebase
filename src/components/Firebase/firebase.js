ff3a6987427dc3a07570661dae84381130e19dfb
Neil <nlahaise@hotmail.com>
2019-04-27 14:43:26 -0700

    add google signin

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -17,6 +17,8 @@ class Firebase {
 
     this.auth = app.auth();
     this.db = app.database();
+
+    this.googleProvider = new app.auth.GoogleAuthProvider();
   }
 
   //*** Auth API ***
@@ -25,6 +27,9 @@ class Firebase {
 
   doSignInWithEmailAndPassword = (email, password) =>
     this.auth.signInWithEmailAndPassword(email, password);
+
+  doSignInWithGoogle = () => 
+    this.auth.SignInWithPopup(this.googleProvider);
 
   doSignOut = () => this.auth.signOut();
 
